e0b4e45c56f884175eac14e83b280401dc5dad14
tejayy <tbj456@gmail.com>
2024-11-23 20:28:00 +0530

    Local Storage sessions

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,11 @@ import todo_icon from "../assets/calendar-plus-solid.svg";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
   const inputRef = useRef();
 
   const add = () => {
@@ -38,7 +42,7 @@ const Todo = () => {
   };
 
   useEffect(() => {
-    console.log(todoList);
+    localStorage.setItem("todos", JSON.stringify(todoList));
   }, [todoList]);
 
   return (
